05591910d57f3ac4597b1c6376a04d087f4843db
CHEGEBB <chegephil24@gmail.com>
2024-05-26 16:03:43 +0300

    new

--- frontend/src/authpages/Signup.js
+++ frontend/src/authpages/Signup.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Assuming you are using React Router for navigation
-import { auth, firestore } from '../firebase'; // Assuming you have a firestore instance for database operations
+import { Link } from 'react-router-dom';
+import { auth, firestore } from '../components/firebase'; 
+import './Signup.scss';
+
+
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
@@ -11,8 +14,6 @@ const SignUp = () => {
     const [emailError, setEmailError] = useState('');
 
     const checkPasswordStrength = (value) => {
-        // Implement your password strength checker logic here
-        // For example, you can check the length, presence of special characters, etc.
         const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/; // Minimum eight characters, at least one uppercase letter, one lowercase letter, and one number
         if (regex.test(value)) {
             setPasswordStrength('strong');
@@ -34,20 +35,15 @@ const SignUp = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            // Check if passwords match
             if (password !== confirmPassword) {
                 setPasswordMatch(false);
                 return;
             }
-
-            // Check if email is already in use
             const emailExists = await checkEmailExists(email);
             if (emailExists) {
                 setEmailError('Email already in use');
                 return;
             }
-
-            // Create user
             await auth.createUserWithEmailAndPassword(email, password);
             alert('User registered successfully');
         } catch (error) {
@@ -56,7 +52,6 @@ const SignUp = () => {
     };
 
     const checkEmailExists = async (email) => {
-        // Query Firestore to check if the email already exists
         const querySnapshot = await firestore.collection('users').where('email', '==', email).get();
         return !querySnapshot.empty;
     };
