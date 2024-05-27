f1fcdfb328b5852db364bf450772f5b0467cb396
CHEGEBB <chegephil24@gmail.com>
2024-05-27 20:06:23 +0300

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.scss';
+import Signup from './authpages/Signup';
 
 function App() {
   return (
     <div className="App">
+    
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Signup />} />
+        {/* <Route path="/login" element={<Login />} /> */}
+      </Routes>
+    </Router>
     </div>
   );
 }
