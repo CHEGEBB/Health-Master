0aa53618a5f64caa7efd3e18ed7d4081d8ac4e57
CHEGEBB <chegephil24@gmail.com>
2024-05-28 19:14:51 +0300

    new

--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -7,9 +7,25 @@ const Profile = () => {
 
   return (
     <div className="profile-container">
-    
-    </div>
+    <div className="personal-details">
+      <div className="profile-pic">
+       {/* profile pic implementation */}
+      </div>
+      <div className="user-details">
+        <div className="user-name">
+          {/* user name implementation */}
+          <h1>Brian</h1>
+        </div>
+        <div className="user-email">
+          {/* user email implementation */}
+        </div>
+
+      </div>
+      </div>
+    </div>   
   );
+  
+    
 };
 
 export default Profile;
