d6c43d778648bcae687c8d9a1e4b6ef31397a1d9
CHEGEBB <chegephil24@gmail.com>
2024-05-28 19:27:18 +0300

    new

--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -20,8 +20,54 @@ const Profile = () => {
           {/* user email implementation */}
           <p>brayo@gmail.com</p>
         </div>
+        <div className="user-phone">
+          {/* user phone number implementation */}
+          <p>0712345678</p>
+        </div>
 
       </div>
+      </div>
+      <div className="health-details">
+      <div className="column1">
+      <div className="gender-info">
+        <h2>Gender</h2>
+        <p>Male</p>
+      </div>
+      <div className="dob-info">
+        <h2>Date of Birth</h2>
+        <p>01/01/2000</p>
+      </div>
+      <div className="blood-type-info">
+        <h2>Blood Type</h2>
+        <p>A+</p>
+      </div>
+      <div className="height-info">
+        <h2>Height</h2>
+        <p>170cm</p>
+      </div>
+      </div>
+      <div className="column2">
+        <div className="weight-info">
+        <h2>Weight</h2>
+        <p>70kg</p>
+        </div>
+        <div className="allergies-info">
+        <h2>Allergies</h2>
+        <p>None</p>
+        </div>
+        <div className="medications-info">
+          <div className="prescribed-drugs">
+            <h2>Prescribed Drugs</h2>
+            <p>None</p>
+          </div>
+        </div>
+        <div className="city-info">
+          <h2>City</h2>
+          <p>Nairobi</p>
+        </div>
+      </div>
+    
+
       </div>
     </div>   
   );
