44687db7db87c78d5ba2a8d9df28bc61d9b9b26e
CHEGEBB <chegephil24@gmail.com>
2024-05-29 18:13:42 +0300

    new

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './Home.scss';
 import { Line } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-} from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, BarElement } from 'chart.js';
 import Heartcon from '../images/icons/bytesize--heart.svg';
+import {  Bar } from 'react-chartjs-2';
+
 
 import Notificationicon from '../images/icons/ic--baseline-notifications.svg';
 import SearchIcon from '../images/icons/fluent--search-48-filled (1).svg';
@@ -25,13 +18,19 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  BarElement
 );
 
 const generateRandomHeartRate = () => Math.floor(Math.random() * (120 - 60 + 1) + 60);
+const generateRandomBloodPressure = () => ({
+  systolic: Math.floor(Math.random() * (150 - 90 + 1) + 90),
+  diastolic: Math.floor(Math.random() * (90 - 60 + 1) + 60)
+});
 
 const Homepage = () => {
   const [heartRateData, setHeartRateData] = useState([{ x: 0, y: generateRandomHeartRate() }]);
+  const [bloodPressureData, setBloodPressureData] = useState(generateRandomBloodPressure());
   const chartRef = useRef(null);
   let xValue = useRef(1);
 
@@ -45,12 +44,13 @@ const Homepage = () => {
         }
         return newData;
       });
+      setBloodPressureData(generateRandomBloodPressure());
     }, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
-  const data = {
+  const heartRateChartData = {
     datasets: [
       {
         label: 'Heart Rate',
@@ -58,6 +58,17 @@ const Homepage = () => {
         fill: false,
         borderColor: '#FF0000',
         tension: 0.1,
+      }
+    ]
+  };
+
+  const bloodPressureChartData = {
+    labels: ['Systolic', 'Diastolic'],
+    datasets: [
+      {
+        label: 'Blood Pressure',
+        data: [bloodPressureData.systolic, bloodPressureData.diastolic],
+        backgroundColor: ['#FF0000', '#0000FF']
       }
     ]
   };
@@ -70,24 +81,24 @@ const Homepage = () => {
         position: 'bottom',
         min: heartRateData.length > 20 ? heartRateData[heartRateData.length - 20].x : 0,
         max: heartRateData.length > 20 ? heartRateData[heartRateData.length - 1].x : 20,
-        grid : {
-            display: false
+        grid: {
+          display: false
         },
         ticks: {
           stepSize: 1,
-          color : 'white'
+          color: 'white'
         }
       },
       y: {
         min: 50,
         max: 130,
 
-        ticks : {
-            color : 'white'
+        ticks: {
+          color: 'white'
         }
-        
+
       },
-  
+
     },
     animation: {
       duration: 1000,
@@ -107,7 +118,7 @@ const Homepage = () => {
       <header>
         <div className="search-bar">
           <img src={SearchIcon} alt="Search" />
-          <input type="text" name="search" placeholder="Search"/>
+          <input type="text" name="search" placeholder="Search" />
         </div>
         <div className="notifications">
           <img src={Notificationicon} alt="Notifications" />
@@ -126,21 +137,29 @@ const Homepage = () => {
       <div className="main">
         <div className="health-stats">
           <h2>My Health Stats</h2>
+          <div className="stats">
           <div className="heart-rate">
-          <div className="heartRate">
-          <div className="rate">
-          <h4>Heart Rate</h4>
-          </div>
-          <div className="heartcon">
-          <img src={Heartcon} alt="Heart Rate" />
-          </div>
-          </div>
+            <div className="heartRate">
+              <div className="rate">
+                <h4>Heart Rate</h4>
+              </div>
+              <div className="heartcon">
+                <img src={Heartcon} alt="Heart Rate" />
+              </div>
+            </div>
             <div className="heart-rate-value">
-                <p>{heartRateData[heartRateData.length - 1].y}/118</p>
+              <p>{heartRateData[heartRateData.length - 1].y}/118</p>
             </div>
             <div className="graph-for-heartrate">
-              <Line ref={chartRef} data={data} options={options} />
+              <Line ref={chartRef} data={heartRateChartData} options={options} />
             </div>
+          </div>
+          <div className="blood-pressure">
+            <h4>Blood Pressure</h4>
+            <div className="graph-for-blood-pressure">
+              <Bar data={bloodPressureChartData} />
+            </div>
+          </div>
           </div>
         </div>
         <div className="my-doctors">
