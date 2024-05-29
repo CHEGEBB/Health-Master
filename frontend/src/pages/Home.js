3ed49ce80a956f2969235d3d6d7bead824c5ad5a
CHEGEBB <chegephil24@gmail.com>
2024-05-29 16:21:42 +0300

    new

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import './Home.scss';
 import Notificationicon from '../images/icons/ic--baseline-notifications.svg';
-import  SearchIcon from '../images/icons/fluent--search-48-filled (1).svg';
+import SearchIcon from '../images/icons/fluent--search-48-filled (1).svg';
 import Remindericon from '../images/icons/hugeicons--apple-reminder.svg';
-
+import ProfilePic from '../images/brian.jpeg';
 
 const Homepage = () => {
-    return (  
-        
+    return (
         <div className='homepage'>
-         <header>
-            <div className="search-bar">
-            <img src={SearchIcon} alt="Search" />
-                <input type="text" name="search" placeholder="Search"/>
+            <header>
+                <div className="search-bar">
+                    <img src={SearchIcon} alt="Search" />
+                    <input type="text" name="search" placeholder="Search"/>
+                </div>
+                <div className="notifications">
+                    <img src={Notificationicon} alt="Notifications" />
+                </div>
+                <div className="reminders">
+                    <img src={Remindericon} alt="Reminders" />
+                </div>
+                <div className="profile">
+                    <img className="profile-pic" src={ProfilePic} alt="Profile" />
+                    <div className="profile-info">
+                        <span className="profile-name">John Doe</span>
+                        <span className="profile-email">john.doe@example.com</span>
+                    </div>
+                </div>
+            </header>
+            <div className="main">
+            <div className="health-stats">
+
             </div>
-            <div className="notifications">
-            <img src={Notificationicon} alt="Notifications" />
+            <div className="my-doctors">
+                
             </div>
-            <div className="reminders">
-            <img src={Remindericon} alt="Reminders" />
             </div>
-            <div className="profile">
-            </div>
-        </header>
-       
         </div>
     );
 }
- 
+
 export default Homepage;
