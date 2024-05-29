99ba0052233fa0dfcc60cc0a58913377c9fdb971
CHEGEBB <chegephil24@gmail.com>
2024-05-29 15:34:42 +0300

    new

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Home.scss';
+import Notificationicon from '../images/icons/ic--baseline-notifications.svg';
+import  SearchIcon from '../images/icons/fluent--search-48-filled (1).svg';
+import Remindericon from '../images/icons/hugeicons--apple-reminder.svg';
 
 
 const Homepage = () => {
     return (  
+        
         <div className='homepage'>
-            <h1>Health Master</h1>
+         <header>
+            <div className="search-bar">
+            <img src={SearchIcon} alt="Search" />
+                <input type="text" name="search" placeholder="Search"/>
+            </div>
+            <div className="notifications">
+            <img src={Notificationicon} alt="Notifications" />
+            </div>
+            <div className="reminders">
+            <img src={Remindericon} alt="Reminders" />
+            </div>
+            <div className="profile">
+            </div>
+        </header>
+       
         </div>
     );
 }
