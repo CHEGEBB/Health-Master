ed37ef7c960b70ce14070560bd2807ab8d65f171
CHEGEBB <chegephil24@gmail.com>
2024-06-03 12:58:55 +0300

    new

--- frontend/src/pages/HealthGoals.js
+++ frontend/src/pages/HealthGoals.js
@@ -7,7 +7,27 @@ const HealthGoals = () => {
 <div className="goals">
 <div className="progress">
     <div className="progress-row1">
-        <div className="weekly-progress">
+        <div className="weekly-progress-workout">
+            <h2>Weekly Progress</h2>
+            <div className="progress-bar">
+                <div className="progress-bar-fill"></div>
+            </div>
+        </div>
+        <div className="weekly-progress-run">
+            <h2>Weekly Progress</h2>
+            <div className="progress-bar">
+                <div className="progress-bar-fill"></div>
+            </div>
+        </div>
+    </div>
+    <div className="progress-row2">
+        <div className="weekly-progress-sleep">
+            <h2>Weekly Progress</h2>
+            <div className="progress-bar">
+                <div className="progress-bar-fill"></div>
+            </div>
+        </div>
+        <div className="weekly-progress-cycling">
             <h2>Weekly Progress</h2>
             <div className="progress-bar">
                 <div className="progress-bar-fill"></div>
