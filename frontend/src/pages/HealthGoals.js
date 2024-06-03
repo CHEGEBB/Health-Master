76b569cf28383ad74f36a46c2bfa3ccb2083280b
CHEGEBB <chegephil24@gmail.com>
2024-06-03 12:30:55 +0300

    new

--- frontend/src/pages/HealthGoals.js
+++ frontend/src/pages/HealthGoals.js
@@ -4,6 +4,23 @@ import './HealthGoals.scss';
 const HealthGoals = () => {
     return(
     <div className="health-goals">
+    <div className="goals">
+        <div className="fitness-goals">
+            <h3>Fitness Goals</h3>
+        </div>
+        <div className="nutritional-goals">
+            <h3>Nutritional Goals</h3>
+        </div>
+        <div className="sleep-goals">
+            <h3>Sleep Goals</h3>
+        </div>
+        <div className="mental-goals">
+            <h3>Mental Health Goals</h3>
+            </div>
+        <div className="social-goals">
+            <h3>Social Goals</h3>
+            </div>
+    </div>
 
     </div>
     );
