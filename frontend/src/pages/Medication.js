c6c418d39eb8c687236a0c53c98de86088caf4ea
CHEGEBB <chegephil24@gmail.com>
2024-06-05 10:17:51 +0300

    new

--- frontend/src/pages/Medication.js
+++ frontend/src/pages/Medication.js
@@ -2,16 +2,7 @@ import React, { useState } from 'react';
 import ApexCharts from 'react-apexcharts';
 import './Medication.scss';
 
-const prescriptions = [
-    { id: 'A348', title: 'Prescription 1', createdBy: 'Dr. Jacob Ryan', date: '12/05/2016', disease: 'Fever' },
-    { id: 'A645', title: 'Prescription 2', createdBy: 'Dr. Rajesh', date: '12/05/2016', disease: 'Cholera' },
-    { id: 'A873', title: 'Prescription 3', createdBy: 'Dr. Jay Soni', date: '12/05/2016', disease: 'Jaundice' },
-    { id: 'A927', title: 'Prescription 4', createdBy: 'Dr. John Deo', date: '12/05/2016', disease: 'Typhoid' },
-    { id: 'A228', title: 'Prescription 5', createdBy: 'Dr. Megha Trivedi', date: '12/05/2016', disease: 'Malaria' },
-    { id: 'A345', title: 'Prescription 6', createdBy: 'Dr. Sarah Smith', date: '12/05/2016', disease: 'Infection' },
-    { id: 'A765', title: 'Prescription 7', createdBy: 'Dr. Jacob Ryan', date: '12/05/2016', disease: 'Fever' },
-    { id: 'A125', title: 'Prescription 8', createdBy: 'Dr. Rajesh', date: '12/05/2016', disease: 'Cholera' },
-];
+
 
 const Medication = () => {
     const [alarms, setAlarms] = useState([]);
@@ -24,6 +15,17 @@ const Medication = () => {
     const [points, setPoints] = useState(0);
     const [showSuccess, setShowSuccess] = useState(false);
 
+    const [prescriptions, setPrescriptions] = useState([
+        { id: 'A348', title: 'Prescription 1', createdBy: 'Dr. Jacob Ryan', date: '12/05/2016', disease: 'Fever' },
+        { id: 'A645', title: 'Prescription 2', createdBy: 'Dr. Rajesh', date: '12/05/2016', disease: 'Cholera' },
+        { id: 'A873', title: 'Prescription 3', createdBy: 'Dr. Jay Soni', date: '12/05/2016', disease: 'Jaundice' },
+        { id: 'A927', title: 'Prescription 4', createdBy: 'Dr. John Deo', date: '12/05/2016', disease: 'Typhoid' },
+        { id: 'A228', title: 'Prescription 5', createdBy: 'Dr. Megha Trivedi', date: '12/05/2016', disease: 'Malaria' },
+        { id: 'A345', title: 'Prescription 6', createdBy: 'Dr. Sarah Smith', date: '12/05/2016', disease: 'Infection' },
+        { id: 'A765', title: 'Prescription 7', createdBy: 'Dr. Jacob Ryan', date: '12/05/2016', disease: 'Fever' },
+        { id: 'A125', title: 'Prescription 8', createdBy: 'Dr. Rajesh', date: '12/05/2016', disease: 'Cholera' },
+    ]);
+    
     const addAlarm = (e) => {
         e.preventDefault();
         if (alarmTime && alarmNote) {
@@ -92,7 +94,35 @@ const Medication = () => {
             setPoints(0);
         }
     };
+    const [newPrescription, setNewPrescription] = useState({
+        id: '',
+        title: '',
+        createdBy: '',
+        date: '',
+        disease: '',
+    });
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewPrescription((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const addPrescription = (e) => {
+        e.preventDefault();
+        if (newPrescription.id && newPrescription.title && newPrescription.createdBy && newPrescription.date && newPrescription.disease) {
+            setPrescriptions([...prescriptions, newPrescription]);
+            setNewPrescription({
+                id: '',
+                title: '',
+                createdBy: '',
+                date: '',
+                disease: '',
+            });
+        }
+    };
     return (
         <div className='medication'>
             <h1>Medication</h1>
@@ -215,6 +245,49 @@ const Medication = () => {
                     ))}
                 </tbody>
             </table>
+                
+            <form onSubmit={addPrescription}>
+                <input
+                    type='text'
+                    name='id'
+                    placeholder='ID'
+                    value={newPrescription.id}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type='text'
+                    name='title'
+                    placeholder='Title'
+                    value={newPrescription.title}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type='text'
+                    name='createdBy'
+                    placeholder='Created By'
+                    value={newPrescription.createdBy}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type='date'
+                    name='date'
+                    value={newPrescription.date}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type='text'
+                    name='disease'
+                    placeholder='Disease'
+                    value={newPrescription.disease}
+                    onChange={handleChange}
+                    required
+                />
+                <button type='submit'>Add Prescription</button>
+            </form>
         </div>
     );
 }
